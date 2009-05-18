d494d9f9bda4249daf94da686688e46ca415a19d
Chromakode <chromakode@gmail.com>
2009-05-17 22:31:35 -0700

    Fix incorrect best depth reading in debugging output.

--- Havoc/Player/IterativeDeepening.hs
+++ Havoc/Player/IterativeDeepening.hs
@@ -13,26 +13,29 @@ import Havoc.State
 iterativelyDeepenC :: (String -> IO ()) -> (State -> Int -> a -> IO (Int, [(Double, Move)], a)) -> a -> NominalDiffTime -> State -> IO (Int, Int, [(Double, Move)])
 iterativelyDeepenC debugLn doSearch startData seconds state
     = do startTime <- getCurrentTime
-         run startTime (1, 0, []) startData
+         run startTime (0, 0, []) startData
          
     where
         runDepth depth continueData = do
             !(!nodes, !moves, !continueData') <- doSearch state depth continueData
             return (nodes, moves, continueData')
         
-        run startTime out@(curDepth, nodes, lastMoves) continueData
+        run startTime out@(lastDepth, nodes, lastMoves) continueData
             = do curTime <- getCurrentTime
                  let elapsed = diffUTCTime curTime startTime
                  let remain = seconds - elapsed
                  let remainMicroseconds = floor (remain * 10^6)
+                 let tryDepth = lastDepth+1
                  if remainMicroseconds < 0
                      then return out
-                     else do result <- timeout remainMicroseconds (runDepth curDepth continueData)
+                     else do result <- timeout remainMicroseconds (runDepth tryDepth continueData)
                              case result of
-                               Nothing                            -> return out
+                               Nothing                            -> do
+                                   debugLn $ "Depth " ++ show tryDepth ++ ": interrupted"
+                                   return out
                                Just (nodes', moves, continueData) -> do
-                                   debugLn $ "Depth " ++ show curDepth ++ ": " ++ showScoredMoves state moves
-                                   run startTime ((curDepth+1), nodes+nodes', moves) continueData
+                                   debugLn $ "Depth " ++ show tryDepth ++ ": " ++ showScoredMoves state moves
+                                   run startTime (tryDepth, nodes+nodes', moves) continueData
 
 iterativelyDeepen :: (String -> IO ()) -> (State -> Int -> IO (Int, [(Double, Move)])) -> NominalDiffTime -> State -> IO (Int, Int, [(Double, Move)])
 iterativelyDeepen debugLn doSearch
