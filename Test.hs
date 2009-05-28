b84aedef034c1e1520c934e1740347b6502a9d77
Chromakode <chromakode@gmail.com>
2009-05-28 10:42:31 -0700

    Found test case where pawn promotion not being valued properly.

--- Test.hs
+++ Test.hs
@@ -1,14 +1,30 @@
 import Control.Monad
 import Control.Monad.ST
+import Random
 import Havoc.Game
+import Havoc.Game.Move
 import Havoc.Game.State
 import Havoc.Game.MiniChess.Game
 import Havoc.Player
+import Havoc.Player.DoUndo
+import Havoc.Player.NegamaxPruned
+
+startBoardText =
+    "k....\n\
+    \....p\n\
+    \.....\n\
+    \.....\n\
+    \p....\n\
+    \....K\n"
 
 testMoveGen = do
-    state <- stToIO $ (startState :: ST RealWorld (MiniChess RealWorld))
+    state <- stToIO $ readBoard startBoardText >>= (\board -> return $ MiniChess $ Evaluated 0 $ GameState 2 Black board)
     moves <- stToIO $ moveGen state
     (stToIO $ (showGameState . gameState) state) >>= putStrLn
     (stToIO $ showMoves state moves) >>= putStrLn
+    
+    stdGen <- getStdGen
+    (nodes, scoredMoves) <- negamaxPrunedMove state 1
+    (stToIO $ showScoredMoves state scoredMoves) >>= putStrLn
     
 main = testMoveGen
