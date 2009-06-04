a25ba51b2a8f55a4f49fb5a5f5b59452a85a1fd7
chromakode <chromakode@gmail.com>
2009-06-03 18:44:29 -0700

    Add move counting to check move generation.

--- Test.hs
+++ Test.hs
@@ -4,21 +4,31 @@ import Random
 import Havoc.Game
 import Havoc.Game.Move
 import Havoc.Game.State
-import Havoc.Game.MiniChess.Game
+import Havoc.Game.MiniChess
 import Havoc.Player
 import Havoc.Player.DoUndo
 import Havoc.Player.NegamaxPruned
 
 startBoardText =
     "kqbnr\n\
-    \p.ppp\n\
+    \ppppp\n\
     \.....\n\
-    \.P.Q.\n\
-    \PP.PP\n\
-    \RNB.K\n"
+    \.....\n\
+    \PPPPP\n\
+    \RNBQK\n"
 
-testMoveGen = do
-    state <- stToIO $ readBoard startBoardText >>= (\board -> return $ MiniChess $ Evaluated 0 $ GameState 2 Black board)
+countMoveGen state = do
+    count <- stToIO $ countMoves state 4
+    putStrLn $ "Node count: " ++ show count
+    where
+        countMoves :: (Game a) => a s -> Int -> ST s Int
+        countMoves state' 0     = return 1
+        countMoves state' depth = do
+            moves <- moveGen state'
+            counts <- mapMoves state' (\m s -> countMoves state' (depth-1)) moves
+            return $ sum counts
+
+testMoveScoring state = do
     moves <- stToIO $ moveGen state
     (stToIO $ (showGameState . gameState) state) >>= putStrLn
     (stToIO $ showMoves state moves) >>= putStrLn
@@ -34,4 +44,6 @@ testMoveGen = do
     
     
     
-main = testMoveGen
+main = do
+    state <- stToIO $ readBoard startBoardText >>= (\board -> return $ MiniChess $ Evaluated 0 $ GameState 2 Black board)
+    countMoveGen state
